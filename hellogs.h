e9946b1464cfa8913d68930cfd25e8dcea79e236
viraltaco <7462994+ViralTaco@users.noreply.github.com>
2020-02-27 03:48:47 +0100

    Removed non-standard `#include <malloc.h>`; Changed `hellog_t` struct name to `hellog_t_impl` so there is no confusion with the typedefed type (another option would be using the typedef struct idiom); clang formatted code using Google style

--- hellogs.h
+++ hellogs.h
@@ -1,20 +1,22 @@
 #ifndef _HELLOGS_H_
 #define _HELLOGS_H_
 
+/*
+#include <malloc.h> NON STANDARD (malloc is defined in stdlib.h) 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <malloc.h>
 
-struct hellog_t {
-	char * filename;
-	FILE * fp;
+struct hellog_t_impl {
+  char* filename;
+  FILE* fp;
 };
 
-typedef struct hellog_t hellog_t;
+typedef struct hellog_t_impl hellog_t;
 
-hellog_t * hellog_create(char * filename);
-void hellog_close(hellog_t * log);
-int hellog_write(hellog_t * log, const char * log_msg);
+hellog_t* hellog_create(char* filename);
+void hellog_close(hellog_t* log);
+int hellog_write(hellog_t* log, const char* log_msg);
 
-#endif //_HELLOGS_H_
+#endif  //_HELLOGS_H_
